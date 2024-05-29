328e1a1c06ad63d96d0452806963f3c3ce3f53c1
filipmarcinkowski <filipmarcinkowski26@gmail.com>
2024-05-29 16:13:30 +0200

    js 1.21

--- script.js
+++ script.js
@@ -13,6 +13,7 @@
 // btnContact.addEventListener('mouseout', changeToYellow);
 
 const imageBox = document.querySelectorAll('.image-box');
+const offerBox = document.querySelectorAll('.offer-item__container');
 
 imageBox.forEach(function (i) {
   i.addEventListener('click', function () {
@@ -21,4 +22,24 @@ imageBox.forEach(function (i) {
     });
     i.classList.add('full-size');
   });
+});
+
+// OFFER DESCRIPTION OPEN / CLOSE FUNCTIONALITY
+
+const showDesc = function (a, b, c, d) {
+  a.classList.toggle('offer-open');
+  b.classList.toggle('cross-1__close');
+  c.classList.toggle('cross-2__close');
+  d.classList.toggle('add-padding');
+};
+
+offerBox.forEach(function (i) {
+  const showOffer = i.querySelector('.offer-more');
+  const offerIconClose1 = i.querySelector('.cross-1');
+  const offerIconClose2 = i.querySelector('.cross-2');
+  const offerDesc = i.querySelector('.offer-more__desc');
+  i.addEventListener(
+    'click',
+    showDesc(showOffer, offerIconClose1, offerIconClose2, offerDesc)
+  );
 });
